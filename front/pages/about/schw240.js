509aade4f652f1e2d9649f19a309aeb3e932ad31
schw240 <schw240@gmail.com>
2021-07-03 23:51:21 +0900

    리액트_노드버드

---
+++ front/pages/about/schw240.js
@@ -0,0 +1,7 @@
+const schw240 = () => {
+    return (
+        <div>Hello, schw240!</div>
+    )
+}
+
+export default schw240;
---
+++ front/pages/index.js
@@ -0,0 +1,13 @@
+import AppLayout from "../components/AppLayout";
+
+const Home = () => {
+    return (
+        <AppLayout>
+            {/* 감싸진애들이 children */}
+            <div>Hello, Next!</div>
+
+        </AppLayout>
+    )
+}
+
+export default Home;
